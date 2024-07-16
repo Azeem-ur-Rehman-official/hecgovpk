d39f5e54b4f03a28200e73bd02031f8438d78a15
Azeem-ur-Rehman-official <azeemcoding@gmail.com>
2024-07-16 12:33:01 +0500

    favicon change

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "hecgovpk",
-  icons:{icon:"https://iconape.com/wp-content/files/ts/195929/svg/195929.svg",}
+  icons:{icon:['/favicon.ico?v4'],
+    apple:['apple-touch-icon.png?v4'],
+    shortcut:['apple-touch-icon.png?v4']
+  }
  
 };
 
@@ -15,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
     <head>
       <link rel="shorcut icon" href="https://iconape.com/wp-content/files/ts/195929/svg/195929.svg"/>
+      <link rel="icon" type="image/svg" href="https://iconape.com/wp-content/files/ts/195929/svg/195929.svg"/>
     </head>
       <body className={inter.className}>{children}</body>
     </html>
